131aca4bb87a5369a1bfc4d618518430849b0a25
Friedjoff Trautwein <mail@friedjoff.de>
2021-11-17 21:08:51 +0100

    add initial Telegram bot

---
+++ src/bot/index.ts
@@ -0,0 +1,10 @@
+import { Bot, webhookCallback } from "https://deno.land/x/grammy/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const bot = new Bot(Deno.env.get("TOKEN") || "");
+
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
+
+const app = new Application();
+
+app.use(webhookCallback(bot, "oak"));
