25f7e03b5178b72a9290e206be76cf6411aefd7b
Friedjoff Trautwein <mail@friedjoff.de>
2021-11-17 21:15:16 +0100

    trigger deploy

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const bot = new Bot(Deno.env.get("TOKEN") || "");
 
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
+bot.on("message", (ctx) => ctx.reply("Hi there!!"));
 
 const app = new Application();
 
