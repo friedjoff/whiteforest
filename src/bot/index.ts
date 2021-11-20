fd6e128fd28b38fbb774927c0be637f133763499
Friedjoff Trautwein <mail@friedjoff.de>
2021-11-20 22:28:04 +0100

    add error logging

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,8 +3,14 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const bot = new Bot(Deno.env.get("TOKEN") || "");
 
-bot.on("message", (ctx) => ctx.reply("Hi there!!"));
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
 
 const app = new Application();
 
+app.addEventListener("error", (evt) => {
+  console.log(evt.error);
+});
+
 app.use(webhookCallback(bot, "oak"));
+
+await app.listen({ port: 8000 });
